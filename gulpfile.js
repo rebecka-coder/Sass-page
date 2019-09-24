df2ae21709cb8661c5c92cc1b5ca8f62305ffdba
rebecka-coder <reho0301@student.miun.se>
2019-09-24 16:58:32 +0200

    new repo

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+//Variabler för olika paket inom gulp
+const { src, dest, watch, series, parallel } = require("gulp");
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+// Sökvägar
+const files = {
+    htmlPath: "src/**/*.html",
+    jsPath: "src/**/*.js",
+    sassPath: "src/**/*.scss",
+    imagePath: "src/**/*.png"
+}
+
+//Task: Sass
+function sassTask() {
+    return src(files.sassPath)
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(dest('pub')
+    );
+}
+// Task: kopiera HTML
+function copyHTML() {
+    return src(files.htmlPath)
+        .pipe(dest('pub'))
+        .pipe(browserSync.stream())
+}
+//Task: kopiera bilder
+function copyImages() {
+    return src(files.imagePath)
+        .pipe(dest('pub'))
+        .pipe(browserSync.stream())
+}
+//Task: Sammanslå jsfiler, minifiera filer
+function jsTask() {
+    return src(files.jsPath)
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(dest('pub/js'))
+        .pipe(browserSync.stream())
+}
+// Task: watcher
+function watchTask() { 
+   browserSync.init({
+       server: {
+           baseDir: 'pub/'
+       }
+   });
+    watch([files.htmlPath, files.jsPath, files.sassPath, files.imagePath],
+    parallel(copyHTML, copyImages, jsTask, sassTask)).on('change', browserSync.reload);
+}
+//Kalla på funktioner
+exports.default = series(
+    sassTask,
+    copyHTML, 
+    copyImages, 
+    jsTask, 
+    watchTask
+);
